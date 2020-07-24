b3a01446f40e14623de838bbb88d8ed5e4920986
David <zhangwj@tcforce.jp>
2020-07-25 02:00:15 +0900

    update Book Lwc

---
+++ force-app/main/default/lwc/firstTestLwc/firstTestLwc.js
@@ -0,0 +1,18 @@
+import { LightningElement,api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import BOOK_OBJECT from '@salesforce/schema/Book__c';
+
+export default class FirstTestLwc extends LightningElement {
+    @api book;
+     /** View Details Handler to navigates to the record page */
+     handleViewDetailsClick() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.book.Id,
+                objectApiName: BOOK_OBJECT.objectApiName,
+                actionName: 'view'
+            }
+        });
+    }
+}
